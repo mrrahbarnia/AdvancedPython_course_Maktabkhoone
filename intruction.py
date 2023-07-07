cd5323e01e1480f0594331245d82b6cab22da519
Mohammadreza <mrrahbarnia@gmail.com>
2023-07-07 14:21:43 +0330

    Connecting to db
    Step1:connecting to our database in mysql with mysql.connector

---
+++ intruction.py
@@ -0,0 +1,7 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(host = '127.0.0.1',
+                              user = '****',
+                              password = '****',
+                              database = 'Project'
+                              )
