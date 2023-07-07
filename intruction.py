7ef045df58d8c05c9a094f3eb7c5af6558203344
Mohammadreza <mrrahbarnia@gmail.com>
2023-07-07 14:28:36 +0330

    Scraping all data
    Step2:in this project i'm gonna use Truecar.com to obtain all required data.
    in this project i only need four parameters(model,year,miles,price).
    send get request for the range that i want and scrap all data with bs4 and put them into my database with mysql.connector

--- intruction.py
+++ intruction.py
@@ -5,3 +5,21 @@ cnx = mysql.connector.connect(host = '127.0.0.1',
                               password = '****',
                               database = 'Project'
                               )
+
+import requests
+from bs4 import BeautifulSoup
+
+for page in range(1,500):
+    url = "https://www.truecar.com/used-cars-for-sale/listings/?buyOnline=true&page=%i" % page 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text , "html.parser")
+    model = soup.find_all("span","truncate")[::2]
+    year = soup.find_all("span",{"class":"vehicle-card-year text-xs"})
+    miles = soup.find_all("div",{"data-test":"vehicleMileage"})
+    price = soup.find_all("div" , "heading-3 my-1 font-bold")
+    for m,y,mi,p in zip(model,year,miles,price):
+        cursor = cnx.cursor(buffered=True)
+        query = "INSERT IGNORE INTO cars (Model,Year,Miles,Price) VALUES(\'%s\',%i,%f,%f);" % ((m.text),(int(y.text)),(float(((mi.text).replace(',','.')).replace('miles',''))),(float(((p.text).replace(',','.')).replace('$',''))))
+        cursor.execute(query)
+        cnx.commit()
+
