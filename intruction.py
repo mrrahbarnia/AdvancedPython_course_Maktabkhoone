0511e291c997cbb1c3da3dfc3a4b2b0f92807347
Mohammadreza <mrrahbarnia@gmail.com>
2023-07-07 14:46:03 +0330

    Fit with DecisionTreeClassifier
    Step3: at the end we have to fit all data in x,y parameters to predict the fair price with DecisionTreeClassifier

--- intruction.py
+++ intruction.py
@@ -23,3 +23,35 @@ for page in range(1,500):
         cursor.execute(query)
         cnx.commit()
 
+from sklearn import tree
+from sklearn.preprocessing import LabelEncoder
+
+car = input("Model: ")
+
+cursor = cnx.cursor(buffered=True)
+query = "SELECT Model,Year,Miles,Price FROM cars WHERE Model = \'%s\'" % car
+cursor.execute(query)
+x = []
+y = []
+for i in cursor:
+    x.append(((i[1]),int(i[2])))
+    y.append(int(i[3]))
+
+cursor = cnx.cursor(buffered=True)
+query = "SELECT Model,Year,Miles,Price FROM cars WHERE Model = \'%s\'" % car
+cursor.execute(query)
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x,y)
+
+for i in cursor:
+    new_data = [[i[1],int(i[2])]]
+    fair_price = clf.predict(new_data)
+    print("Model: {}|Year: {}|Miles: {}|Price: {}|Fair price: {}".format(i[0],i[1],i[2],i[3],fair_price[0]))
+
+
+cnx.commit()
+cnx.close()
+
+
+
